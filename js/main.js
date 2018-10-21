0914b9abe87975e1ba89a0d30705f8a4e5b4508f
bexaca <33089360+bexaca@users.noreply.github.com>
2018-10-21 18:18:41 +0200

    changes

--- js/main.js
+++ js/main.js
@@ -6,4 +6,65 @@ jQuery(document).ready(function () {
             scrollTop: $(this.hash).offset().top
         }, 1000, "swing");
     });
+
+    $('a[href*=\\#].top').on('click', function (event) {
+        event.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 1000, "swing");
+    });
+
+    var $header = $('header nav'),
+        scrollClass = 'on-scroll',
+        activateAtY = 20;
+
+    function deactivateHeader() {
+        if (!$header.hasClass(scrollClass)) {
+            $header.addClass(scrollClass);
+        }
+    }
+
+    function activateHeader() {
+        if ($header.hasClass(scrollClass)) {
+            $header.removeClass(scrollClass);
+        }
+    }
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > activateAtY) {
+            deactivateHeader();
+            $('.top').addClass('show');
+        } else {
+            activateHeader();
+            $('.top').removeClass('show');
+        }
+    });
+
+    var oNama = $("#o-nama").offset().top - 280;
+    var treneri = $("#treneri").offset().top - 280;
+    var treninzi = $("#treninzi").offset().top - 280;
+    var galerija = $("#galerija").offset().top - 280;
+    var kontakt = $("#kontakt").offset().top - 280;
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > oNama && $(window).scrollTop() < treneri) {
+            $('nav ul li a[href*=\\#o-nama]').addClass('hover');
+            $('nav ul li a').not('nav ul li a[href*=\\#o-nama]').removeClass('hover');
+        } else if ($(window).scrollTop() > treneri && $(window).scrollTop() < treninzi) {
+            $('nav ul li a[href*=\\#treneri]').addClass('hover');
+            $('nav ul li a').not('nav ul li a[href*=\\#treneri]').removeClass('hover');
+        } else if ($(window).scrollTop() > treninzi && $(window).scrollTop() < galerija) {
+            $('nav ul li a[href*=\\#treninzi]').addClass('hover');
+            $('nav ul li a').not('nav ul li a[href*=\\#treninzi]').removeClass('hover');
+        } else if ($(window).scrollTop() > galerija && $(window).scrollTop() < kontakt) {
+            $('nav ul li a[href*=\\#galerija]').addClass('hover');
+            $('nav ul li a').not('nav ul li a[href*=\\#galerija]').removeClass('hover');
+        } else if ($(window).scrollTop() > kontakt) {
+            $('nav ul li a[href*=\\#kontakt]').addClass('hover');
+            $('nav ul li a').not('nav ul li a[href*=\\#kontakt]').removeClass('hover');
+        } else {
+            $('nav ul li a').removeClass('hover');
+        }
+    });
+
 });
